468ab8567789139e2e47fc00fcda3fcc2fa6487d
Andreas Jung <yet@gmx.de>
2016-02-02 12:51:14 +0100

    updated

--- test.py
+++ test.py
@@ -6,8 +6,10 @@ root = lxml.html.fromstring(open('index.html', 'rb').read())
 for num, expr in enumerate(root.xpath('//math')):
     mml = lxml.html.tostring(expr)
     mml = mml.replace('<math display="block">', '<math xmlns="https://www.w3.org/1998/Math/MathML/">')
-    open('{}.mml'.format(num), 'wb').write(mml)
-
+    with open('{}.mml'.format(num), 'wb') as fp:
+        fp.write('<article xmlns="http://docbook.org/ns/docbook" version="5.0"><title>Area enclosed by a circle</title> <equation>\n')
+        fp.write(mml)
+        fp.write('</equation></article>\n')
 
     cmd = 'java -jar saxon9he.jar -s:{}.mml -xsl:xsl/invoke-mml2tex.xsl  --suppressXsltNamespaceCheck:on'.format(num)
     print num+1, cmd
